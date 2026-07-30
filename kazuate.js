a0ddeb2a4cb6c70eee05cb8ee69f5deb09e33a7a
Hori Yuma <258482@st.takushoku-u.ac.jp>
2026-07-30 23:07:02 +0900

    情報メディア　最終課題

--- kazuate.js
+++ kazuate.js
@@ -1,59 +1,61 @@
 // 課題4-1: 数当てゲーム
 
 // 乱数を使って正解を作る
-let kotae = Math.floor(Math.random()*10) + 1;
-console.log('答え（デバッグ用）: ' + kotae);
+let kotae = Math.floor(Math.random() * 10) + 1;
+console.log("答え（デバッグ用）: " + kotae);
 
-// 入力回数（予想回数）
-let kaisu2 = 0;
-// そのほか，必要に応じて変数を宣言してもよい
-let b,p;
-// ボタンを押した後の処理をする関数 hantei() の定義
-let d =document.querySelector("button#print");
+// 正解の数、予想回数
+let kaisu = 0;
+let owari = false;
+
+// ボタン
+let d = document.querySelector("button#print");
+
+// ボタンを押した後の処理をする関数
 function hantei() {
-  // ここから: 予想回数を1増やして，span#kaisu 要素のテキストを更新
-  let kaisu =document.querySelector("span#kaisu");
-  kaisu2=kaisu2+1;
-  // ここまで: 予想回数を1増やして，span#kaisu 要素のテキストを更新
-  
-  // ここから: テキストボックスに指定された数値を yoso に代入する
-  b=document.querySelector('input[name="kaitou"]');
-  yoso=Number(b.value);
-  // ここまで: テキストボックスに指定された数値を yoso に代入する
-  
-  // ここから: 正解判定する
-  // 　　　　  正解/不正解のときのメッセージを表示する 
-  
-  if(kaisu2<4){
-    if(kaisu2===3 && yoso===kotae) {
-        p =document.querySelector('p#result');
-        p.textContent="Congratulation!";
-    }else if(yoso===kotae){
-      kaisu.textContent=kaisu2;
-      p =document.querySelector('p#result');
-      p.textContent="Congratulation!";
-    }else {
-      kaisu.textContent=kaisu2;
-      if(kaisu2===3) {
-        p =document.querySelector('p#result');
-        p.textContent="もう挑戦できないよ！正解は"+kotae;
-      }else{
-        kaisu.textContent=kaisu2;
-        if(kotae<yoso){
-          p =document.querySelector('p#result');
-          p.textContent="残念、その数字より下";
-        }else{
-          p =document.querySelector('p#result');
-          p.textContent="残念、その数字より上";
-        }
-      }
+
+  // 回数表示用
+  let span = document.querySelector("span#kaisu");
+
+  // 結果表示用
+  let p = document.querySelector("p#result");
+
+  // 入力された予想
+  let b = document.querySelector('input[name="kaitou"]');
+  let yoso = Number(b.value);
+
+  // 4回以上、または正解済みなら終了
+  if (kaisu >= 3 || owari) {
+    p.textContent = "答えは " + kotae + " でした．すでにゲームは終わっています";
+    return;
+  }
+
+  // 予想回数を1増やす
+  kaisu++;
+  span.textContent = kaisu;
+
+  // 正解判定
+  if (yoso === kotae) {
+    p.textContent = "正解です．おめでとう!";
+    owari = true;
+  } else {
+
+    // 3回目で不正解
+    if (kaisu === 3) {
+      p.textContent = "まちがい．残念でした．答えは " + kotae + " です．";
+    }
+
+    // 予想が正解より小さい
+    else if (yoso < kotae) {
+      p.textContent = "まちがい．答えはもっと大きいですよ";
+    }
+
+    // 予想が正解より大きい
+    else {
+      p.textContent = "まちがい．答えはもっと小さいですよ";
     }
   }
-  
-  // ここまで: 正解判定する
 }
-d.addEventListener('click', hantei);
 
-// ここから: ボタンを押した時のイベントハンドラとして hantei を登録
-
-// ここまで: ボタンを押した時のイベントハンドラとして hantei を登録
+// ボタンを押した時のイベントハンドラとして登録
+d.addEventListener("click", hantei);
